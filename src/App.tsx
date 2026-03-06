51e1903aac655942396ae87f78fb3d27ce1a3a50
Rob <info+apple@rhod.com>
2026-03-06 14:37:04 -0600

    fix: remove unused import for vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function App() {
   return (
     <div style={{ backgroundColor: '#0A0A0A', color: 'white', minHeight: '100vh', fontFamily: 'sans-serif' }}>
